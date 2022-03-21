48514911f0d768a7271dc78ca5a35e6fcccfbd67
Edson Caliman <edsoncaliman@gmail.com>
2022-03-21 19:20:20 -0300

    Solution, folders and initial projects

---
+++ Banking.Operation.Transfer.Query.CrossCutting.Ioc/Modules/DataModule.cs
@@ -0,0 +1,20 @@
+﻿using Net.Core.Template.Infra.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Net.Core.Template.CrossCutting.Ioc.Modules
+{
+
+    public static class DataModule
+    {
+        public static void Register(IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            var serverVersion = new MySqlServerVersion(new Version(8, 0, 26));
+
+            services.AddDbContext<AppDbContext>(options => options.UseMySql(connectionString, serverVersion));
+        }
+    }
+}
---
+++ Banking.Operation.Transfer.Query.CrossCutting.Ioc/Modules/DomainModule.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Net.Core.Template.CrossCutting.Ioc.Modules
+{
+    public static class DomainModule
+    {
+        public static void Register(this IServiceCollection services)
+        {
+
+        }
+    }
+}
